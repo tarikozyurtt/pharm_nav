c163b9f198c04face0cc3905364f6e131c5ef3ac
emre-yrdkl <emreyurdakul24@gmail.com>
2023-12-30 19:49:14 +0300

    writing sign up page

---
+++ admin-panel/src/pages/SignupPage.js
@@ -0,0 +1,103 @@
+// src/SignupPage.js
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const RegisterUser = async (body) => {
+  return await fetch('https://splendorous-praline-960c1f.netlify.app/.netlify/functions/index/authenticate', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: body,
+  });
+};
+
+function SignupPage({ setIsLoggedIn }) {
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    location: '',
+    photos: [],
+  });
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+
+  const handleFileChange = (e) => {
+    setUserData({ ...userData, photos: [...userData.photos, ...e.target.files] });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await RegisterUser(
+      JSON.stringify({
+        name: userData.name,
+        email: userData.email,
+        password: userData.password,
+        location: userData.location
+      })
+    )
+
+    const result = await response.json();
+    
+    // Registration logic here
+    setIsLoggedIn(true);
+    navigate('/dashboard');
+  };
+
+  const handleLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const locationStr = `Lat: ${position.coords.latitude}, Lon: ${position.coords.longitude}`;
+        setUserData({ ...userData, location: locationStr });
+      }, () => {
+        alert('Unable to retrieve your location');
+      });
+    } else {
+      alert('Geolocation is not supported by this browser.');
+    }
+  };
+
+  return (
+    <div className="space-y-4">
+
+      <div className=' my-20'>
+        <p className='text-center font-bold text-3xl text-[#6F70FF]'>
+          PharmNav Admin Panel
+        </p>
+      </div>
+
+      <div className='grid grid-cols-1 w-1/6 mx-auto'>
+
+        <div className='mb-4'>
+          <input type="text" name="name" placeholder="Name" onChange={handleInputChange} className="p-2 border border-gray-300 rounded w-full" />
+        </div>
+        <div className='mb-4'>
+          <input type="email" name="email" placeholder="Email" onChange={handleInputChange} className="p-2 border border-gray-300 rounded w-full" />
+        </div>
+        <div className='mb-4'>
+          <input type="password" name="password" placeholder="Password" onChange={handleInputChange} className="p-2 border border-gray-300 rounded w-full" />
+        </div>
+        <div className='mb-4'>
+          <input type="text" readOnly={true} name="location" placeholder="Current Location" value={userData.location} onChange={handleInputChange} className="p-2 border border-gray-300 rounded w-1/2 mr-5" />
+          <button type="button" onClick={handleLocation} className="py-2 px-4 bg-blue-500 text-white rounded w-2/5">Get Loc.</button>
+        </div>
+        <div className='mb-4'>
+          <input type="file" multiple onChange={handleFileChange} className="p-2 border border-gray-300 rounded w-full" />
+        </div>
+        <div className='mb-4 justify-center flex'>
+          <button onClick={handleSubmit} className="py-2 px-4 bg-green-500 text-white rounded w-1/2 ">Sign Up</button>
+        </div>
+
+      </div>
+    </div>
+    
+
+  );
+}
+
+export default SignupPage;
