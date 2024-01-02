87d4f2d3c52c50023a3e4c5e0395852828d82751
ozyurt19 <ozyurt19@itu.edu.tr>
2024-01-02 03:33:30 +0300

    feat: add profile screen

--- client/src/screens/ProfileScreen/ProfileScreen.js
+++ client/src/screens/ProfileScreen/ProfileScreen.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import { useAuth } from '../../AuthContext';
 
 const getHistory = async (body) => {
@@ -12,8 +12,29 @@ const getHistory = async (body) => {
   });
 };
 
+const PharmacyItem = ({ code, drugs, navigation }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => {navigation.replace("Dashboard", code)}}>
+      <Text style={styles.code}>{code}</Text>
+      <View style={styles.drugsContainer}>
+        {Object.entries(drugs).map(([drugName, quantity]) => (
+          <View key={drugName} style={styles.drugItem}>
+            <Text style={styles.drugName}>{drugName}</Text>
+            <Text style={styles.quantity}>{`Quantity: ${quantity}`}</Text>
+          </View>
+        ))}
+      </View>
+      </TouchableOpacity>
+    </View>
+    
+  );
+};
+
 export default function ProfileScreen({ navigation }) {
   const { user, signOut } = useAuth();
+  const [history, setHistory] = useState([])
+
   useEffect(() => {
     console.log(user.userId)
 
@@ -27,7 +48,8 @@ export default function ProfileScreen({ navigation }) {
           //   Alert.alert('Sign Up Failed', result)
           //   throw new Error('Sign up failed');
           // }
-          console.log("profile res: ", result)
+          console.log("profile res: ", result.pastPrescriptions)
+          setHistory(result.pastPrescriptions)
         })
 
     } catch (error) {
@@ -49,25 +71,32 @@ export default function ProfileScreen({ navigation }) {
     return true
   };
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Profile Screen</Text>
+    <View style={styles.main}>
+      <ScrollView style={styles.slider}>
+      {history.map((data, index) => (
+        <PharmacyItem key={index} {...data} navigation={navigation} />
+      ))}
       <TouchableOpacity
         style={styles.button}
         onPress={handleLogOut}
       >
         <Text style={styles.buttonText}>Log Out</Text>
       </TouchableOpacity>
+      </ScrollView>
     </View>
 
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  slider: {
+    width: "100%",
   },
   text: {
     fontSize: 20,
@@ -75,13 +104,46 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: '#6f70ff',
     padding: 10,
-    margin: '4%',
+    margin: '8%',
     width: '80%',
     borderRadius: 5,
+    justifyContent: 'center'
   },
   buttonText: {
     color: 'white',
     fontSize: 16,
     textAlign: 'center',
+  },
+  container: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 10,
+    padding: 15,
+    margin: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  code: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  drugsContainer: {
+    flexDirection: 'column',
+  },
+  drugItem: {
+    marginBottom: 8,
+  },
+  drugName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  quantity: {
+    fontSize: 14,
   },
 });
