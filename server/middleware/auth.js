8eb32bef62aae991a618ddec99ee27b93665b7d8
ituitis20-caner19 <caner19@itu.edu.tr>
2024-01-02 23:52:35 +0300

    logging token info

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/users');
 
-const auth= async (req, res, next) => {
+const auth = async (req, res, next) => {
 
 
-  if(!req.headers.authorization){
+  if (!req.headers.authorization) {
     return res.status(401).json({ message: 'Unauthorized' });
   }
   const token = req.headers.authorization.split(' ')[1];;
-//   console.log(req);
+  //   console.log(req);
 
   // Check if the token exists
   if (!token) {
@@ -18,11 +18,15 @@ const auth= async (req, res, next) => {
   try {
     // Verify the token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("decoded is")
+    console.log(decoded)
+    console.log("decoded user id")
+    console.log(decoded.userId)
     // console.log(decoded);
     // Add the decoded user information to the request object
     const user = await User.findById(decoded.userId);
-    if(!user){
-    return res.status(400).json({ message: 'User does not exist' });
+    if (!user) {
+      return res.status(400).json({ message: 'User does not exist' });
 
     }
     req.user = user;
@@ -35,4 +39,4 @@ const auth= async (req, res, next) => {
   }
 };
 
-module.exports=auth
+module.exports = auth
