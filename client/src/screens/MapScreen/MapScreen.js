5c0b5bce4d75a13ca3e3019ca6e763c55cc3fa95
ozyurt19 <ozyurt19@itu.edu.tr>
2024-01-03 23:42:15 +0300

    fix: fix maps for android

--- client/src/screens/MapScreen/MapScreen.js
+++ client/src/screens/MapScreen/MapScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Linking, Platform, Image  } from 'react-native';
+import { View, Text, StyleSheet, Linking, Platform, Image } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 
@@ -8,51 +8,62 @@ export default function MapScreen({ route }) {
     const pharmLocation = {
         latitude: prop.coordinates[1],
         longitude: prop.coordinates[0]
-    }  
+    }
     const [location, setLocation] = useState(null);
     const [mid, setMid] = useState(null);
 
     const handlePress = (latitude, longitude) => {
-        const googleMapsURL = `comgooglemaps://?daddr=${latitude},${longitude}`;
-        const appleMapsURL = `http://maps.apple.com/?daddr=${latitude},${longitude}`;
-    
-        Linking.canOpenURL(googleMapsURL)
-          .then((supported) => {
-            if (supported) {
-              return Linking.openURL(googleMapsURL);
-            } else {
-              return Linking.openURL(appleMapsURL);
-            }
-          })
-          .catch((err) => console.error('An error occurred', err));
-      };
+        if (Platform.OS === 'ios') {
+            const appleMapsURL = `http://maps.apple.com/?daddr=${latitude},${longitude}`;
 
-      
+            Linking.canOpenURL(googleMapsURL)
+                .then((supported) => {
+                    if (supported) {
+                        return Linking.openURL(googleMapsURL);
+                    } else {
+                        return Linking.openURL(appleMapsURL);
+                    }
+                })
+                .catch((err) => console.error('An error occurred', err));
+        }
+    };
+
+
     useEffect(() => {
         (async () => {
             // console.log("pharm loc -> ", pharmLocation)
 
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== 'granted') {
-                Alert.alert('Error', 'Permission to access location was denied.');
-                return;
-            }
+            Location.requestForegroundPermissionsAsync()
+            .then(async prop => {
+                let { status } = prop
+                if (status !== 'granted') {
+                    Alert.alert('Error', 'Permission to access location was denied.');
+                    return;
+                }
 
-            let loc = await Location.getCurrentPositionAsync({});
+                await Location.getCurrentPositionAsync({})
+                .then(async prop => {
 
-            setLocation({
-                latitude: loc.coords.latitude,
-                longitude: loc.coords.longitude,
-                latitudeDelta: 0.03,
-                longitudeDelta: 0.02
-            });
+                    setLocation({
+                        latitude: prop.coords.latitude,
+                        longitude: prop.coords.longitude,
+                        latitudeDelta: 0.03,
+                        longitudeDelta: 0.02
+                    });
+                    console.log(location)
 
-            setMid({
-                latitude: (loc.coords.latitude + pharmLocation.latitude) / 2,
-                longitude: (loc.coords.longitude + pharmLocation.longitude) / 2,
-                latitudeDelta: 0.1,
-                longitudeDelta: 0.1
+                    setMid({
+                        latitude: (prop.coords.latitude + pharmLocation.latitude) / 2,
+                        longitude: (prop.coords.longitude + pharmLocation.longitude) / 2,
+                        latitudeDelta: 0.1,
+                        longitudeDelta: 0.1
+                    });
+                })
+                .catch(error => {
+                    Alert.alert('Maps Failed!', "An error occurred while getting location. Please try again.");
+                })
             })
+
         })();
     }, []);
 
@@ -61,9 +72,9 @@ export default function MapScreen({ route }) {
             {location ? (
                 <MapView style={styles.map} region={mid}>
                     <Marker coordinate={location} title="You are here" />
-                    
+
                     <Marker coordinate={pharmLocation} title="Your target pharmacy" onPress={() => handlePress(prop.coordinates[1], prop.coordinates[0])}>
-                        <Image source={require('D:/pharmnavPush/pharm_nav/client/assets/pharm_icon.jpg')} style={{height: 35, width:35 }} />
+                        <Image source={require('../../../assets/pharm_icon.jpg')} style={{ height: 35, width: 35 }} />
                     </Marker>
                 </MapView>
             ) : (
