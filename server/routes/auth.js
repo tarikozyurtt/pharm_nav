9a213e2676d15ecf06a3b23c8c5d4e032b408143
ozgursefer <116740803+seferog@users.noreply.github.com>
2024-01-03 22:19:19 +0300

    Try catch blocks

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,41 +7,53 @@ const router = express.Router();
 require("dotenv").config();
 
 router.post("/authenticate", async (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  try {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
 
-  await connectDB();
-  const { email, password } = req.body;
-  if (!email) {
-    return res.status(400).json({ message: '"email" is required' });
+    await connectDB();
+    const { email, password } = req.body;
+    if (!email) {
+      return res.status(400).json({ message: '"email" is required' });
+    }
+    if (!password) {
+      return res.status(400).json({ message: '"password" is required' });
+    }
+
+    const user = await User.findOne({ email: email });
+    // If the user doesn't exist or the password is incorrect, return an error
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Email or password is incorrect" });
+    }
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if (!validPassword) {
+      return res
+        .status(401)
+        .json({ message: "Email or password is incorrect" });
+    }
+
+    // Generate a JWT token with the user ID as payload
+
+    const token = jwt.sign({ user }, process.env.JWT_SECRET);
+
+    // Return the token as JSON
+    res.status(200).json({
+      userToken: token,
+      userInfo: {
+        userName: user.name,
+        userEmail: user.email,
+        userId: user._id,
+      },
+    });
+  } catch {
+    res.status(500).send("Server error");
   }
-  if (!password) {
-    return res.status(400).json({ message: '"password" is required' });
-  }
-
-  const user = await User.findOne({ email: email });
-  // If the user doesn't exist or the password is incorrect, return an error
-  if (!user) {
-    return res.status(401).json({ message: "Email or password is incorrect" });
-  }
-  const validPassword = await bcrypt.compare(password, user.password);
-
-  if (!validPassword) {
-    return res.status(401).json({ message: "Email or password is incorrect" });
-  }
-
-  // Generate a JWT token with the user ID as payload
-  
-  const token = jwt.sign({ user }, process.env.JWT_SECRET);
-
-  // Return the token as JSON
-  res.status(200).json({
-    userToken: token,
-    userInfo: { userName: user.name, userEmail: user.email, userId: user._id },
-  });
 });
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -29,7 +29,9 @@ router.post("/registerPatient", async (req, res) => {
   if (!name || !email || !password || !userRole) {
     return res
       .status(400)
-      .send('Validation failed: Name, email, password, and userRole are required');
+      .send(
+        "Validation failed: Name, email, password, and userRole are required"
+      );
   }
   await connectDB();
   const newUser = new User({
@@ -78,145 +80,158 @@ router.post("/history", auth, async (req, res) => {
 
 //
 router.post("/registerPharmacist", async (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  await connectDB();
-  const newUser = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    userRole: req.body.userRole,
-    pharmacyName: req.body.pharmacyName,
-  });
   try {
-    await newUser.save();
-  } catch (error) {
-    if (error.code === 11000) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    await connectDB();
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      userRole: req.body.userRole,
+      pharmacyName: req.body.pharmacyName,
+    });
+    try {
+      await newUser.save();
+    } catch (error) {
+      if (error.code === 11000) {
+        // MongoDB duplicate key error
+        return res.status(400).send("Email already exists");
+      } else {
+        return res.status(500).send(error);
+      }
+    }
+
+    const newPharmacy = new Pharmacy({
+      name: req.body.pharmacyName,
+      location: req.body.location,
+      ownerId: newUser._id,
+      description: req.body.description,
+      address: req.body.address,
+      phoneNum: req.body.phoneNum,
+    });
+    try {
+      console.log(newUser._id);
+      console.log(req.body.location);
+      await newPharmacy.save();
+    } catch (error) {
       // MongoDB duplicate key error
-      return res.status(400).send("Email already exists");
-    } else {
+
       return res.status(500).send(error);
     }
+
+    // Return the new user as JSON
+    res.status(200).json({
+      pharmacyId: newPharmacy._id,
+      userId: newUser._id,
+      userName: newUser.name,
+      userEmail: newUser.email,
+      pharmacyName: newUser.pharmacyName,
+      location: newPharmacy.location,
+    });
+  } catch (err) {
+    console.log(err);
   }
-
-  const newPharmacy = new Pharmacy({
-    name: req.body.pharmacyName,
-    location: req.body.location,
-    ownerId: newUser._id,
-    description: req.body.description,
-    address: req.body.address,
-    phoneNum: req.body.phoneNum,
-  });
-  try {
-    console.log(newUser._id);
-    console.log(req.body.location);
-    await newPharmacy.save();
-  } catch (error) {
-    // MongoDB duplicate key error
-
-    return res.status(500).send(error);
-  }
-
-  // Return the new user as JSON
-  res.status(200).json({
-    pharmacyId: newPharmacy._id,
-    userId: newUser._id,
-    userName: newUser.name,
-    userEmail: newUser.email,
-    pharmacyName: newUser.pharmacyName,
-    location: newPharmacy.location,
-  });
 });
 
 router.post("/sendticket", auth, async (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  await connectDB();
-  const { email, pharmacyName, pharmacistName, description } = req.body;
-  const newTicket = new ticketSchema({
-    email: email,
-    pharmacyName: pharmacyName,
-    pharmacistName: pharmacistName,
-    description: description,
-  });
   try {
-    await newTicket.save();
-  } catch (error) {
-    console.log(error);
-    if (error.code === 11000) {
-      // MongoDB duplicate key error
-      return res.status(400).send("Ticket already exists");
-    } else {
-      return res.status(500).send(error);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    await connectDB();
+    const { email, pharmacyName, pharmacistName, description } = req.body;
+    const newTicket = new ticketSchema({
+      email: email,
+      pharmacyName: pharmacyName,
+      pharmacistName: pharmacistName,
+      description: description,
+    });
+    try {
+      await newTicket.save();
+    } catch (error) {
+      console.log(error);
+      if (error.code === 11000) {
+        // MongoDB duplicate key error
+        return res.status(400).send("Ticket already exists");
+      } else {
+        return res.status(500).send(error);
+      }
     }
+    res.status(200).json({
+      email: newTicket.email,
+      pharmacyName: newTicket.pharmacyName,
+      pharmacistName: newTicket.pharmacistName,
+      description: newTicket.description,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Server Error");
   }
-  res.status(200).json({
-    email: newTicket.email,
-    pharmacyName: newTicket.pharmacyName,
-    pharmacistName: newTicket.pharmacistName,
-    description: newTicket.description,
-  });
 });
 
 router.post("/image", auth, async (req, res) => {
-  console.log("here");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  const formatted = formidable();
-  formatted.parse(req, async (err, fields, files) => {
-    if (!fields?.pharmacyId)
-      return res.status(400).send("Pharmacy id is required");
-    try {
-      const file = await fs.readFile(files.file.filepath);
-      var form = new FormData();
-      form.append("file", file);
-      let config = {
-        method: "post",
-        maxBodyLength: Infinity,
-        url: "https://api.cloudflare.com/client/v4/accounts/ef12f5cfc8aa66a02afc9df9185e3d97/images/v1",
-        headers: {
-          Authorization: "Bearer 3rcWJpvU00LP3tejFLTxFlFeWt0k3dSqBWt3u4qp",
-          ...form.getHeaders(),
-        },
-        data: form,
-      };
-      axios
-        .request(config)
-        .then(async (response) => {
-          res.send({
-            message: "success",
-            image: response.data.result.variants[0],
+  try {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    const formatted = formidable();
+    formatted.parse(req, async (err, fields, files) => {
+      if (!fields?.pharmacyId)
+        return res.status(400).send("Pharmacy id is required");
+      try {
+        const file = await fs.readFile(files.file.filepath);
+        var form = new FormData();
+        form.append("file", file);
+        let config = {
+          method: "post",
+          maxBodyLength: Infinity,
+          url: "https://api.cloudflare.com/client/v4/accounts/ef12f5cfc8aa66a02afc9df9185e3d97/images/v1",
+          headers: {
+            Authorization: "Bearer 3rcWJpvU00LP3tejFLTxFlFeWt0k3dSqBWt3u4qp",
+            ...form.getHeaders(),
+          },
+          data: form,
+        };
+        axios
+          .request(config)
+          .then(async (response) => {
+            res.send({
+              message: "success",
+              image: response.data.result.variants[0],
+            });
+            await connectDB();
+            await pharmacySchema.updateOne(
+              { _id: fields.pharmacyId },
+              { $push: { pharmImages: response.data.result.variants[0] } }
+            );
+          })
+          .catch((error) => {
+            console.log(error);
+            console.log("error");
+            res.status(500).send({ message: "error", image: "error" });
           });
-          await connectDB();
-          await pharmacySchema.updateOne(
-            { _id: fields.pharmacyId },
-            { $push: { pharmImages: response.data.result.variants[0] } }
-          );
-        })
-        .catch((error) => {
-          console.log(error);
-          console.log("error");
-          res.status(500).send({ message: "error", image: "error" });
-        });
-    } catch (error) {
-      console.log(error);
-      console.log("error");
+      } catch (error) {
+        console.log(error);
+        console.log("error");
 
-      res.status(500).send({ message: "error", image: "error" });
-    } finally {
-      // Delete uploaded file
-      await fs.unlink(files.file.filepath);
-    }
-  });
+        res.status(500).send({ message: "error", image: "error" });
+      } finally {
+        // Delete uploaded file
+        await fs.unlink(files.file.filepath);
+      }
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({ message: "error", image: "error" });
+  }
 });
 
 router.get("/user", auth, async (req, res) => {
@@ -235,20 +250,25 @@ router.get("/user", auth, async (req, res) => {
 });
 
 router.post("/getCodeDrugs", auth, async (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  await connectDB();
-  const { code } = req.body;
-  const drugInfo = await codeSchema.findOne({ code: code });
+  try {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    await connectDB();
+    const { code } = req.body;
+    const drugInfo = await codeSchema.findOne({ code: code });
 
-  if (!drugInfo) {
-    return res.status(400).send("Prescription not found");
+    if (!drugInfo) {
+      return res.status(400).send("Prescription not found");
+    }
+
+    res.status(200).json({ drugs: drugInfo?.drugs ?? [] });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Server Error");
   }
-
-  res.status(200).json({ drugs: drugInfo?.drugs ?? [] });
 });
 
 module.exports = router;
